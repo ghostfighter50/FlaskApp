235be6016bcdf4656f2e3d037c7a2543ea21a867
Prosperify <prosperify.ai@gmail.com>
2025-02-04 10:48:31 +0100

    Clean logs and add production server and add env example

--- run.py
+++ run.py
@@ -1,19 +1,13 @@
-# run.py
-
 import logging
 from app import create_app
+from waitress import serve
 
 def main() -> None:
-    """
-    Entry point for the Flask application.
-    Initializes and runs the Flask app.
-    """
     app = create_app()
 
-    # Configure logging for the run script
     logging.basicConfig(level=logging.INFO)
     app.logger.info("Starting Flask application.")
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    serve(app, host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
